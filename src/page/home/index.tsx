3808bd3ad1b44eb2e25340a9c123566d4e768938
maye <maye@alu120.com>
2021-07-06 16:16:15 +0800

    初来乍到

---
+++ src/page/home/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Button } from "antd";
+
+export const Home = React.memo(() => {
+  return (
+    <div>
+      <Button type="primary">按钮</Button>
+    </div>
+  );
+});
---
+++ src/page/userList/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const UserList = React.memo(() => {
+  return <div>Userlist</div>;
+});
